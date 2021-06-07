b3c62664262a362703dc3365e4c61dbed6cd6b8f
Andrey L <forw39@gmail.com>
2021-06-07 02:22:07 +0200

    changed styles

--- src/views/templates/home/home.js
+++ src/views/templates/home/home.js
@@ -3,22 +3,21 @@ import "./home.scss";
 const cardTriggers = document.querySelectorAll(".cards__info");
 const inners = document.querySelectorAll(".cards__inner");
 const icons = document.querySelectorAll(".cards__icon");
+const descBottom = document.querySelectorAll(".desc__item");
+const descTriggers = document.querySelectorAll(".desc__top");
+const descImg = document.querySelectorAll(".desc__img");
 
 cardTriggers.forEach((cardTrigger, index) => {
   cardTrigger.addEventListener("click", function () {
-    const faqInner = this.nextElementSibling;
-    if (faqInner.style.maxHeight) {
-      faqInner.style.maxHeight = null;
+    if (inners[index].style.maxHeight) {
+      inners[index].style.maxHeight = null;
       icons[index].style.transform = "rotate(" + 0 + "deg)";
     } else {
-      faqInner.style.maxHeight = faqInner.scrollHeight + "px";
+      inners[index].style.maxHeight =  100 + "%";
       icons[index].style.transform = "rotate(" + 180 + "deg)";
     }
   });
 });
-
-const descTriggers = document.querySelectorAll(".desc__top");
-const descImg = document.querySelectorAll(".desc__img");
 
 descTriggers.forEach((descTrigger, index) => {
   descTrigger.addEventListener("click", function () {
@@ -26,9 +25,11 @@ descTriggers.forEach((descTrigger, index) => {
     if (faqInner.style.maxHeight) {
       faqInner.style.maxHeight = null;
       descImg[index].style.transform = "rotate(" + 0 + "deg)";
+      descBottom[index].style.overflow = 'hidden';
     } else {
-      faqInner.style.maxHeight = faqInner.scrollHeight + "px";
+      faqInner.style.maxHeight = faqInner.scrollHeight + "%";
       descImg[index].style.transform = "rotate(" + 180 + "deg)";
+      descBottom[index].style.overflow = 'visible';
     }
   });
 });
